9706a01f602d164729ae48d63445599b047c792b
Henrique Melo <hmelo2509@gmail.com>
2022-05-07 22:11:13 +0000

    Adicionar arquivos de projeto.

---
+++ ListaLeitura.App/Program.cs
@@ -0,0 +1,31 @@
+﻿using ListaLeitura.App.Repositorio;
+using ListaLeitura.App.Negocio;
+using Microsoft.AspNetCore.Hosting;
+using System;
+
+namespace ListaLeitura.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var _repo = new LivroRepositorioCSV();
+
+            IWebHost host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+
+            //ImprimeLista(_repo.ParaLer);
+            //ImprimeLista(_repo.Lendo);
+            //ImprimeLista(_repo.Lidos);
+        }
+
+        static void ImprimeLista(ListaDeLeitura lista)
+        {
+            Console.WriteLine(lista);
+        }
+    }
+}
